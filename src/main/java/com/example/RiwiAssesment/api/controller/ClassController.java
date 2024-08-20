e449cb1b22c3ba109e0eccaef83c7981a95eda17
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-08-20 13:27:49 -0500

    Find Class by id

--- src/main/java/com/example/RiwiAssesment/api/controller/ClassController.java
+++ src/main/java/com/example/RiwiAssesment/api/controller/ClassController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,19 @@ public class ClassController
     )
     {
         return ResponseEntity.ok().body(this.service.findAllByNameDescription(title,description,page-1,size));
+    }
+
+    @Operation(summary = "Retrieves information about a specific resource.")
+    @ApiResponse(responseCode = "400", description = "Resource not found.",      
+        content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorsResponse.class))
+        }
+    )
+    @GetMapping("/{id}")
+    public ResponseEntity<ClassResponse> getById(@PathVariable String id) {
+        return ResponseEntity.ok().body(
+            this.service.findById(id)
+        );
     }
 
 }
